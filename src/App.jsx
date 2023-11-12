a54ed648d5591cf2ad87af4ef91aa5ba8e58ba0f
marianaborda <marianaborda80@gmail.com>
2023-11-12 10:27:20 -0300

    funcionalidad de botones incremento y decremeto y cambios estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Voto from './component/Voto/Voto'
-import './App.css'
+import React from "react";
+import Voto from "./component/Voto/Voto";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <p className='titulo'>Boca de urnas</p>
+      <p className="titulo">Boca de Urnas</p>
       <div>
-        <Voto name="candidato 1"cont="3"></Voto>
-        <Voto name="candidato 2"cont="2"></Voto>
+        <Voto name="Candidato 1" cont="0" />
+        <Voto name="Candidato 2" cont="0" />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Voto/Voto.jsx
+++ src/component/Voto/Voto.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import './Voto.css'
+import React, { useState } from "react";
+import "./Voto.css";
 
-const Voto =(datos) =>{
-  return(
-    <div class="contenedor">
+const Voto = (datos) => {
+  const [contador, setContador] = useState(Number(datos.cont));
+
+  const incrementarContador = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementarContador = () => {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  };
+
+  return (
+    <div className="contenedor">
       <p>{datos.name}</p>
-      <label htmlFor="">{datos.cont}</label>
-      <div class="contenedor2">
-        <button class="boton">-</button>
-        <button class="boton">+</button>
+      <label htmlFor="">{contador}</label>
+      <div className="contenedor2">
+        <button className="boton" onClick={decrementarContador}>
+          -
+        </button>
+        <button className="boton" onClick={incrementarContador}>
+          +
+        </button>
       </div>
-      
     </div>
-  )
-}
-export default Voto
+  );
+};
+
+export default Voto;
